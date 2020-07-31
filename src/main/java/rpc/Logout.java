e86351229b8218e44848105c7edcf06c1dbe15cf
yuriao <lujiac1@uci.edu>
2020-07-30 18:44:51 -0700

    2nd commit
    add additional functions, finish frontend

--- src/main/java/rpc/Logout.java
+++ src/main/java/rpc/Logout.java
@@ -30,7 +30,7 @@ public class Logout extends HttpServlet {
         if (session != null) {
             session.invalidate();
         }
-        response.sendRedirect("index.jsp");
+        response.sendRedirect("index.html");
 
 	}
 
--- src/main/java/rpc/RecommendItem.java
+++ src/main/java/rpc/RecommendItem.java
@@ -1,13 +1,19 @@
 package rpc;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import entity.Item;
+import recommendation.Recommendation;
 
 /**
  * Servlet implementation class RecommendItem
@@ -28,11 +34,25 @@ public class RecommendItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			response.setStatus(403);
+			return;
+		}
+		String userId = request.getParameter("user_id");
+
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+
+		Recommendation recommendation = new Recommendation();
+		List<Item> items = recommendation.recommendItems(userId, lat, lon);
 		JSONArray array = new JSONArray();
-		array.put(new JSONObject().put("username", "abcd").put("time","060230").put("kalafa","06023230"));
-		array.put(new JSONObject().put("hsereee", "1234"));
-		array.put(new JSONObject().put("msernsd", "12333"));
-		RpcHelper.writeJsonArray(response, array);
+		for (Item item : items) {
+			array.put(item.toJSONObject());
+		}
+		//array.put(new JSONObject().put("Status","OK"));
+		RpcHelper.writeJsonArray(response, array);	
+
 	}
 
 	/**
